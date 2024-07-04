9b2962af6ac81aebefe9ce6152f2e4fdcf1596c8
Nikhil Thakur <nt34542@gmail.com>
2024-07-04 08:17:52 +0530

    Changes

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -6,7 +6,6 @@ const Marquee = () => {
         <div className='  text-white ' ><h1>WHO WE ARE ?</h1></div>
         <div className='border-t border-b mt-6 text-[13vw]  font-extrabold text-white'>
         <h1 >WE ARE DEVELOPERS </h1>
-        <h1 >WE ARE DEVELOPERS </h1>
         </div>
 
     </div>
