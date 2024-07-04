f6f3d28055042e578a39df3a0b8d5ec12f1d284b
Nikhil Thakur <nt34542@gmail.com>
2024-07-04 08:17:18 +0530

    First commit till marquee start

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+    <div className='bg-[#014d43] h-[30vw] w-full rounded-t-lg overflow-x-scroll'>
+        <div className='  text-white ' ><h1>WHO WE ARE ?</h1></div>
+        <div className='border-t border-b mt-6 text-[13vw]  font-extrabold text-white'>
+        <h1 >WE ARE DEVELOPERS </h1>
+        <h1 >WE ARE DEVELOPERS </h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default Marquee
