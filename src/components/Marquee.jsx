a441b09279672a239d6aec3fb9c53127bcd05c2f
Nikhil Thakur <nt34542@gmail.com>
2024-07-04 08:25:35 +0530

    Changes

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Marquee = () => {
   return (
-    <div className='bg-[#014d43] h-[30vw] w-full rounded-t-lg overflow-x-scroll'>
-        <div className='  text-white ' ><h1>WHO WE ARE ?</h1></div>
-        <div className='border-t border-b mt-6 text-[13vw]  font-extrabold text-white'>
-        <h1 >WE ARE DEVELOPERS </h1>
-        </div>
-
+    <div className='bg-[#014d43] h-[30vw] w-full rounded-t-lg overflow-x-auto'>
+      <div className='text-white p-2'>
+        <h1>WHO WE ARE ?</h1>
+      </div>
+      <div className='border-t border-b mt-6 text-[18vw] font-extrabold text-white whitespace-nowrap flex'>
+        <h1 className=''>WE ARE DEVELOPERS</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Marquee
+export default Marquee;
